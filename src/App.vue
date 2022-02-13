01dd8d86285eb80b8b9a2b18f627afeb77bbb050
callum buttery <callumbuttery@icloud.com>
2022-02-13 17:38:07 +0000

    Feat: changes logo

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,12 @@
     <v-app-bar app color="#5c5c5c" elevation="10" height="125">
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="/assets/textwhite.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
           alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
+          class="shrink ml-10 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          src="./assets/ButteryVIsualsText.png"
+          width="125"
         />
       </div>
 
