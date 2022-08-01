58dba1d51c594810be7deb1c7559408c6e45fede
callum buttery <callumbuttery@icloud.com>
2022-08-01 20:36:30 +0100

    Feat: add nav bar buttons

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,34 +1,62 @@
 <template>
-  <v-app-bar app color="primary" dark>
+  <v-app-bar app color="white" dark>
     <div class="d-flex align-center">
       <v-img
         alt="Vuetify Logo"
         class="shrink mr-2"
         contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        src="../assets/logo.png"
         transition="scale-transition"
-        width="40"
-      />
-
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
+        width="180"
       />
     </div>
 
     <v-spacer></v-spacer>
-
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
+    <div v-for="item in items">
+      <v-btn class="ma-1" elevation="2" plain raised color="black">{{ item.text }}</v-btn>
+    </div>
   </v-app-bar>
 </template>
+
+<script>
+export default {
+  name: "Nav",
+  data() {
+    return {
+      items: [
+        {
+          text: "Home",
+          icon: "mdi-home",
+          href: "/",
+        },
+        {
+          text: "About",
+          icon: "mdi-information",
+          href: "/about",
+        },
+        {
+          text: "Contact",
+          icon: "mdi-email",
+          href: "/contact",
+        },
+        {
+          text: "Wildlife",
+          icon: "mdi-account",
+          href: "/login",
+        },
+        {
+          text: "weddings",
+          icon: "mdi-account-plus",
+          href: "/register",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.list {
+  display: inline-block;
+}
+</style>
