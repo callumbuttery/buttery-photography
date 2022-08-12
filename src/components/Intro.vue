b4440d7491fab5946d1d2e9c7b709e848a356d43
Callum Buttery <callumbuttery@icloud.com>
2022-08-12 14:25:37 +0100

    Feat: add colours to full page

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-container fluid wrap>
+  <v-container fluid wrap class="introContainer">
     <v-col align="center" justify="center" class="topLine">───────────────</v-col>
     <v-row>
       <v-col align="center" justify="center" class="introMainHeader">Natural, documentary Wedding Photography</v-col>
     </v-row>
-    <v-col align="center" class="introMainText">Hello! I'm Callum Buttery, Wedding Photographer,
-      Software Engineer and lover of all things sport & wildlife. My wedding style is very relaxed with a focus on
-      catching candid smiles from beautiful moments. Shooting this way allows for everyone to enjoy their day without
-      the stress of having a
-      camera in their face. Lets be real, posing doesn't truly represent your big day!</v-col>
+    <v-col align="center">
+      <div class="introMainText">Hello! I'm Callum Buttery, Wedding Photographer,
+        Software Engineer and lover of all things sport & wildlife. My wedding style is very relaxed with a focus on
+        catching candid smiles from beautiful moments. Shooting this way allows for everyone to enjoy their day without
+        the stress of having a
+        camera in their face. Lets be real, posing doesn't truly represent your big day!
+      </div>
+    </v-col>
 
-    <v-rating length="5" size="40" :value="5" color="warning" align="center" justify="center" class="introRating"
+    <v-rating length="5" size="40" :value="5" color="#f5cb5c" align="center" justify="center" class="introRating"
       readonly>
     </v-rating>
     <div class="introMainText text-caption">9 reviews</div>
@@ -32,30 +35,35 @@ export default {
 .topLine {
   font-size: 60px;
   font-family: "Playfair Display", serif;
-  color: Black;
+  color: #f5cb5c;
   margin-top: 80px;
 }
 
 .bottomLine {
   font-size: 60px;
   font-family: "Playfair Display", serif;
-  color: Black;
+  color: #f5cb5c;
   margin-bottom: 80px;
 }
 
 .introMainHeader {
   font-size: 30px;
   font-family: "Playfair Display", serif;
-  color: Black;
+  color: white;
 }
 
 .introMainText {
   width: 700px;
   margin: auto;
   text-align: center;
+  color: white;
 }
 
 .introRating {
   margin-top: 30px;
+}
+
+.introContainer {
+  background: #1D1C1A !important;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-app-bar app height="100px" color="black" dark>
+  <v-app-bar app height="100px" color="##1D1C1A" dark>
     <v-container fill-height fluid>
       <v-row align="center" justify="center">
         <div v-for="item in items" class="list">
-          <v-btn class="buttons white--text" plain x-large color="black">{{
+          <v-btn class="buttons white--text" text x-large color="black">{{
             item.text
           }}</v-btn>
         </div>
@@ -50,6 +50,7 @@ export default {
 
 .list {
   display: inline-block;
+  color: white;
 }
 
 .buttons {
