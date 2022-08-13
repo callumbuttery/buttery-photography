c8a352a06167e3f87e05e591b3245b3f20c42440
Callum Buttery <callumbuttery@icloud.com>
2022-08-13 11:32:56 +0100

    Feat: add basic form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,8 +6,27 @@
             </v-row>
             <v-col align="center" justify="center" class="contactLine">───────────────</v-col>
             <v-card class="contactCard" elevation="5">
-                <v-row lign="center" justify="center"> 
-                    <v-card-title>Hello world</v-card-title>
+                <v-row lign="center" justify="center">
+                    <v-card-subtitle class="text-center contactText">Please feel free to use the below contact from to get in touch
+                        about any queries you
+                        have. We aim to get back to you within 48 hours</v-card-subtitle>
+
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                        <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required>
+                        </v-text-field>
+
+                        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+                        <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" label="Item"
+                            required></v-select>
+
+                        <v-checkbox v-model="checkbox" :rules="[v => !!v || 'You must agree to continue!']"
+                            label="Do you agree?" required></v-checkbox>
+
+                        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+                            submit
+                        </v-btn>
+                    </v-form>
                 </v-row>
             </v-card>
         </v-container>
@@ -17,6 +36,39 @@
 <script>
 export default {
     name: "Contact",
+    data: () => ({
+        valid: true,
+        name: '',
+        nameRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        ],
+        email: '',
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        select: null,
+        items: [
+            'Basic Package',
+            'Standard Package',
+            'Premium Package',
+            'Other',
+        ],
+        checkbox: false,
+    }),
+
+    methods: {
+        validate() {
+            this.$refs.form.validate()
+        },
+        reset() {
+            this.$refs.form.reset()
+        },
+        resetValidation() {
+            this.$refs.form.resetValidation()
+        },
+    },
 }
 </script>
 
@@ -45,6 +97,11 @@ export default {
 
 .contactCard {
     width: 700px;
+    margin: auto;
+}
+
+.contactText {
+    max-width: 600px;
     margin: auto;
 }
 </style>
