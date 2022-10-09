807080d3b1d5e60d6eafd9635b8c83eecba7f5b1
callum buttery <callumbuttery@icloud.com>
2022-10-09 15:15:42 +0100

    Feat: add Aos to vue

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueScrollTo from 'vue-scrollto';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
  
 Vue.use(VueScrollTo)
 
 Vue.config.productionTip = false
 
 new Vue({
+  created() {
+    AOS.init({
+      anchorPlacement: 'top-bottom',
+    });
+  },
   router,
   store,
   vuetify,
